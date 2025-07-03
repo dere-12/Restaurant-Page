e838aa4a8599be96760dd467bcc376bdf35b5d27
Dereje <dereje2012k@gmail.com>
2025-07-03 06:30:59 +0300

    Add html-loader and page layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import "./styles.css";
-import restaurantImg from "./restaurant.jpg";
 
 console.log("Webpack is Working!");
-
-const h1 = document.querySelector("h1");
-h1.textContent = "My Restaurant";
-
-const img = document.createElement("img");
-img.src = restaurantImg;
-document.querySelector("body").appendChild(img);
